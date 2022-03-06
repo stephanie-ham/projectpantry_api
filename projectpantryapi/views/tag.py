e910eb995e2a3a4b425483017d97147f367dadd1
Stephanie Hamilton <hamiltons621@gmail.com>
2022-03-06 13:09:19 -0600

    feat: view tags by current user

--- projectpantryapi/views/tag.py
+++ projectpantryapi/views/tag.py
@@ -1,3 +1,4 @@
+from rest_framework.decorators import action
 from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
 from drf_yasg.utils import swagger_auto_schema
@@ -6,15 +7,15 @@ from projectpantryapi.models import Tag
 from projectpantryapi.serializers import TagSerializer
 
 class TagView(ViewSet):
+
     @swagger_auto_schema(responses={
         200: openapi.Response(
-            description="The list of tags",
+            description="The list of tags created by current user",
             schema=TagSerializer(many=True)
         )
     })
     def list(self, request):
-        """Get a list of tags"""
-        tags = Tag.objects.all()
+        """Get a list of tags for the current user"""
+        tags = Tag.objects.filter(created_by=request.auth.user)
         serializer = TagSerializer(tags, many=True)
         return Response(serializer.data)
-    
