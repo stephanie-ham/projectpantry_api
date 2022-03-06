28e3acd42a8b4a3f10ab079d24c290861c99e75e
Stephanie Hamilton <hamiltons621@gmail.com>
2022-03-06 12:37:26 -0600

    feat: view all tags

--- projectpantryapi/views/__init__.py
+++ projectpantryapi/views/__init__.py
@@ -1,2 +1,3 @@
 # from .auth import login_user
 from .auth import register_user, login_user
+from .tag import TagView
--- projectpantryapi/views/tag.py
+++ projectpantryapi/views/tag.py
@@ -1,13 +1,20 @@
 from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
-from rest_framework import serializers
+from drf_yasg.utils import swagger_auto_schema
+from drf_yasg import openapi
 from projectpantryapi.models import Tag
+from projectpantryapi.serializers import TagSerializer
 
 class TagView(ViewSet):
-    
-    def retrieve(self, request):
+    @swagger_auto_schema(responses={
+        200: openapi.Response(
+            description="The list of tags",
+            schema=TagSerializer(many=True)
+        )
+    })
+    def list(self, request):
+        """Get a list of tags"""
         tags = Tag.objects.all()
-        
-        serializer = TagSerializer()
+        serializer = TagSerializer(tags, many=True)
         return Response(serializer.data)
     
