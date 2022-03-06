a43f83983a4380fc37347bf495021f037f853853
Stephanie Hamilton <hamiltons621@gmail.com>
2022-03-06 14:05:28 -0600

    route: food view

--- projectpantryapi/urls.py
+++ projectpantryapi/urls.py
@@ -1,10 +1,10 @@
 from rest_framework.routers import DefaultRouter
-# from rest_framework.authtoken import views as auth_token_views
 from django.urls import path, include
 from projectpantryapi import views
 
 router = DefaultRouter(trailing_slash=False)
 router.register(r'tags', views.TagView, 'tag')
+router.register(r'foods', views.FoodView, 'food')
 
 urlpatterns = [
     path('', include(router.urls)),
