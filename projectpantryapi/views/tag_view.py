4610ff10addd6a0f0baab041035957e1f46dabdc
Stephanie Hamilton <hamiltons621@gmail.com>
2022-03-12 19:51:39 -0600

    FIX: merge conflict

--- projectpantryapi/views/tag_view.py
+++ projectpantryapi/views/tag_view.py
@@ -67,3 +67,4 @@ class TagView(ViewSet):
             return Response(None, status=status.HTTP_204_NO_CONTENT)
         except Tag.DoesNotExist as ex:
             return Response({'message': ex.args[0]}, status=status.HTTP_400_BAD_REQUEST)
+
