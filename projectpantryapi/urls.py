d7b75dcdd0c05a83b2c81722faeeec6c10459442
Stephanie Hamilton <hamiltons621@gmail.com>
2022-03-14 02:21:47 -0500

    feat: added quantity to urls

--- projectpantryapi/urls.py
+++ projectpantryapi/urls.py
@@ -3,9 +3,10 @@ from django.urls import path, include
 from projectpantryapi import views
 
 router = DefaultRouter(trailing_slash=False)
-router.register(r'tags', views.TagView, 'tag')
 router.register(r'foods', views.FoodView, 'food')
 router.register(r'locations', views.LocationView, 'location')
+router.register(r'quantities', views.QuantityView, 'quantity')
+router.register(r'tags', views.TagView, 'tag')
 
 urlpatterns = [
     path('', include(router.urls)),
