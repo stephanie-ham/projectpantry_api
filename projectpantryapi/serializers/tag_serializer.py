3af89d26bb0b404fa1ea33ab57c018e90c50f092
Stephanie Hamilton <hamiltons621@gmail.com>
2022-03-16 22:26:45 -0500

    feat: add tag to food

--- projectpantryapi/serializers/tag_serializer.py
+++ projectpantryapi/serializers/tag_serializer.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
 from projectpantryapi.models import Tag
+from projectpantryapi.models.food_tag import FoodTag
 
 
 class TagUserSerializer(serializers.ModelSerializer):
@@ -15,7 +16,17 @@ class TagSerializer(serializers.ModelSerializer):
         model = Tag
         fields = ('id', 'label', 'created_by')
         depth = 1
+        
+class FoodTagSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = FoodTag
+        fields = ('id', 'tag', 'food')
+        depth = 1
 
 class CreateTagSerializer(serializers.Serializer):
     label = serializers.CharField()
-    
+
+class AddTagToFoodSerializer(serializers.Serializer):
+    tag = serializers.IntegerField()
+    food = serializers.IntegerField()
